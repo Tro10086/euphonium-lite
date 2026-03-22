6319aeb862c1070101a2d33257ed9008d2c44aaa
Tro10086 <1547954274@qq.com>
2026-03-22 22:00:58 +0800

    chore: init Vue 3 + Element Plus + auto-import

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <el-button type="primary">Euphonium</el-button>
+    <TheWelcome />
+  </main>
+</template>
